3c3f0d6ab4272dbca6946ff081abac6993d13381
Mohammed Ansar Usman <mohammedansarusman@gmail.com>
2024-06-12 23:16:14 +0400

    api called and stored in redux store named movie

--- src/Browse.js
+++ src/Browse.js
@@ -1,13 +1,28 @@
 import { signOut } from "firebase/auth";
 import {auth} from "./utils/firbase"
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from 'react-router-dom';
 import AuthUpdate from "./AuthUpdate";
+import {API_OPTIONS} from "./utils/constants"
+import {useEffect} from "react";
+import { addNowPlayingMovies } from "./utils/redux/movieSlice";
 
 const Browse = () =>{
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+
     const netflixUser = useSelector((globalStore)=>globalStore.user.userName)
     console.log("user name:",netflixUser)
+
+    const getNowPlayingMovies = async()=>{
+        const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=10', API_OPTIONS)
+        const data = await response.json();
+        console.log(data);
+        dispatch(addNowPlayingMovies(data.results))
+
+    }
+    useEffect(()=>{getNowPlayingMovies()},[])
+
     const handleSignout = () =>{
         signOut(auth);
 
---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Mzg4YjU3MzQxZWE1ZDljODJjMWZmZGVmODc3NzcwNyIsInN1YiI6IjY2NjlkNjQyNGMyMDcxYWJlYWJlYWVkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.51CaROYPWiJM7V-2WwarUmdpB1cxzEb8NK6cPTj1bak'
+    }
+  };
