b5291c0803848092785a5ca685b9323aa9bc5e3f
Mohammed Ansar Usman <mohammedansarusman@gmail.com>
2024-07-18 21:35:49 +0400

    responsive header wip

--- src/Browse.js
+++ src/Browse.js
@@ -43,26 +43,30 @@ const Browse = () => {
     }
 
     const handleSearchIcon = () => {
-        const mName=movie_name.current.value;
+        const mName = movie_name.current.value;
         mName && setDisplayDiv(true);
         !mName && setDisplayDiv(false);
 
-    
+
     }
 
     return (
         <div className="flex flex-col">
             {console.log("Browse component rendered")}
-            <div className="w-full flex justify-center items-center fixed z-20">
+            <div className="bg-red-800 flex flex-col
+                md:bg-gray-900 md:w-full md:flex-row md:justify-center md:items-center md:fixed md:z-20
+                lg:bg-black lg:w-full lg:flex-row lg:justify-center lg:items-center lg:fixed lg:z-20">
                 <AuthUpdate />
-                <div className="w-[25%] h-[100px] bg-black flex flex-row justify-center items-center">
+                <div className="w-[20%] h-[100px] flex flex-row justify-center items-center bg-blue-500">
                     <h1 className="text-red-500">Movie App</h1>
                 </div>
-                <div className="w-[420px] h-[50px] bg-black border-2 border-slate-500 flex absolute rounded-full items-center border-1 py-2 px-1 flex">
+                <div className="lg:w-[380px] lg:h-[50px] lg:border-2 lg:border-slate-500 lg:flex lg:absolute lg:rounded-full lg:items-center lg:border-1 lg:py-2 lg:px-1 lg:bg-pink-500
+                                md:w-[250px] md:h-[50px] md:border-2 md:border-slate-500 md:flex md:absolute md:rounded-full md:items-center md:border-1 md:py-2 md:px-1 ">
                     <input
-                        ref = {movie_name}
+                        ref={movie_name}
                         placeholder="Enter Movie name to search..."
-                        className="w-[90%]  h-full pl-[15px] bg-black focus:outline-none text-gray-500"
+                        className="lg:w-[90%]  lg:h-full lg:pl-[15px] lg:bg-black lg:focus:outline-none lg:text-gray-500
+                                   md:w-[90%]  md:h-full md:pl-[15px] md:bg-black md:focus:outline-none md:text-gray-500 text-sm"
                     // onChange={(e) => e.target.value.length > 0 ? setShowSearchMovies(true) : (setShowSearchMovies(false), setDisplayDiv(false))}
                     />
                     <div
@@ -75,8 +79,9 @@ const Browse = () => {
                         </img>
                     </div>
                 </div>
-                <div className="w-[75%] h-[100px] bg-black flex justify-end items-center pr-[20px]">
-                    <h1 className="text-white mr-5">{netflixUser}</h1>
+                <div className="lg:w-[80%] lg:h-[100px] lg:flex lg:flex-row lg:justify-end lg:items-center lg:pr-[20px] lg:bg-green-400
+                                md:w-[80%] md:h-[100px] md:flex md:flex-col  md:items-end justify-center md:pr-[20px] md:bg-green-800">
+                    <h1 className="text-white lg:mr-5">{netflixUser}</h1>
                     <button
                         className="w-[100px] h-[40px] bg-red-500"
                         onClick={handleSignout}
@@ -86,7 +91,7 @@ const Browse = () => {
             <MainContainer />
             <SecondaryContainer />
             <div className="flex justify-center absolute w-full ">
-                {displayDiv && <SearchMovie update = {()=>setDisplayDiv(false)}/>}
+                {displayDiv && <SearchMovie update={() => setDisplayDiv(false)} />}
             </div>
         </div>
     )
